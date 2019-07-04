eca579f79c2f6edaf207d53445f133ebf8ff7f30
demian44 <demi-44@hotmail.com>
2019-07-04 10:47:35 -0300

    Add Scrolling component

---
+++ parte 2/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Text,
+  ScrollView
+} from 'react-native';
+
+export default class MyText extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+        <Text style={{fontSize: 80 }}>asd</Text>
+      </ScrollView>
+    );
+  }
+}
