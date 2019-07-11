e38e68dcfe044d3748a6207948a1c401a6742d11
demian44 <demi-44@hotmail.com>
2019-07-11 10:21:43 -0300

    Modal, Images and storage

--- parte 2/App.js
+++ parte 2/App.js
@@ -3,7 +3,10 @@ import {
   View,
   Button,
   Alert,
-  StyleSheet
+  StyleSheet,
+  Modal,
+  Image,
+  AsyncStorage
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -22,6 +25,22 @@ const styles = StyleSheet.create({
 
 
 export default class MyText extends React.Component {
+  state = { modalVisible: false };
+  changeModalVisibility = async () => {
+    this.setState({ modalVisible: !this.state.modalVisible });
+    await AsyncStorage.setItem("Data","El datoasdasdasdsa");
+  };
+
+  constructor(props) {
+    super(props);
+    this.getStorageData();
+  }
+
+  getStorageData = async () => {
+    let data = await AsyncStorage.getItem("Data");
+    Alert.alert("Data",data);
+  }
+
   doSomething = () => {
     Alert.alert("The title", "Some text....", [
       { text: "Button 1", onPress: () => { Alert.alert("Button 1...") } },
@@ -31,10 +50,26 @@ export default class MyText extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button title="El Boton" onPress={this.doSomething} />
+      <View style={[styles.container, { backgroundColor: 'gray' }]}>
+        <Modal
+          transparent={true}
+          visible={this.state.modalVisible}
+          animationType="slide"
+        >
+          <View style={[styles.container, { backgroundColor: 'cyan', margin: 70 }]}>
+            <Button title="Modal Button" onPress={this.changeModalVisibility} />
+          </View>
+        </Modal>
+        <Button title="Show..." onPress={this.changeModalVisibility} />
+        <Image
+          source={{ uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg" }}
+          style={{ width: 193, height: 110 }}
+        />
+        <Image
+          source={require("./assets/icon.png")}
+          style={{ width: 193, height: 110 }}
+        />
       </View>
     );
-
   }
 }
